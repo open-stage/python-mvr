2fdd0ed7f2f29140eb3150d1dc5aa0ab047a96df
vanous <petr@linuks.cz>
2024-11-09 09:19:23 +0100

    Update CI

--- pymvr/__init__.py
+++ pymvr/__init__.py
@@ -6,7 +6,7 @@ import sys
 import uuid as py_uuid
 from .value import Matrix, Color  # type: ignore
 
-__version__ = "0.3.0.dev0"
+__version__ = "0.4.0"
 
 
 def _find_root(pkg: "zipfile.ZipFile") -> "ElementTree.Element":
